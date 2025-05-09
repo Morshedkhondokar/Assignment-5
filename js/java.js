f01462b7a77ee4a185ae298282713c3c4ea63301
Morshedkhondokar <morshedkhondokar99@gmail.com>
2025-05-09 17:20:31 +0600

    jascript popup add

--- js/java.js
+++ js/java.js
@@ -7,3 +7,50 @@ if(pageName === 'index.html'){
 if(pageName === 'history.html'){
     document.querySelector('.history').classList.add('active')
 }
+
+//  number make function 
+// function getNumber(id){
+//     const value = document.getElementById(id);
+//     console.log(value)
+//     const number = parseFloat(value);
+//     return number;
+// }
+
+
+
+
+
+
+
+
+
+document.getElementById('Show_popup').addEventListener('click', function(event){
+    event.preventDefault();
+    // get the donation input
+    const Input = document.getElementById('donation_input').value;  
+    const inputvalue = parseFloat(Input);
+    localStorage.setItem('donation',inputvalue);
+    
+    if(!isNaN(inputvalue)){
+        
+            localStorage.getItem('donation');
+            const p = document.createElement('p');
+            p.innerText = `Add: ${inputvalue}`
+            console.log(p)
+            // histore post 
+            const container = document.getElementById('History_container');
+            container.appendChild(p);
+
+        // show popup
+        document.getElementById('Popup').classList.remove('hidden');
+    }else{
+        alert('please enter a valid number')
+    }
+
+})
+
+document.getElementById('hidden_popup').addEventListener('click', function(){
+    document.getElementById('Popup').classList.add('hidden');
+})
+
+ 
